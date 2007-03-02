d60abd9b00e5eaf815fdb7dd72aae85e08b2c28b
senthilkumar <senthilkumar@ca590b3f-1f1f-0410-8945-8120b6f64072>
2007-03-02 09:08:34 +0000

    Skip if lyrics already present

--- trunk/iTuneslyrics/Source/iLyrics.cs
+++ trunk/iTuneslyrics/Source/iLyrics.cs
@@ -25,6 +25,11 @@ namespace iTuneslyrics
         private void btnAlbums_Click(object sender, EventArgs e)
         {
             IITTrackCollection selectedTracks = iTunesApp.SelectedTracks;
+            if ((selectedTracks == null))
+            {
+                MessageBox.Show("Nothing seems to be selected");
+                return;
+            }
 
             lyricsWiki = new org.lyricwiki.LyricWiki();
 
@@ -35,15 +40,25 @@ namespace iTuneslyrics
             }
             else
             {
+                int updatedSongsCount = 0;
                 for (int i = 1; i <= selectedTracks.Count; i++)
                 {
+                    IITFileOrCDTrack currentTrack = (IITFileOrCDTrack)selectedTracks[i];
+                    if (currentTrack.Lyrics != null)
+                        continue;
+
+                    updatedSongsCount++;
                     ManualUpdate ab = new ManualUpdate();
-                    ab.currentTrack = (IITFileOrCDTrack)selectedTracks[i];
+                    ab.currentTrack = currentTrack;
                     ab.lyricsWiki = lyricsWiki;
                     DialogResult dr = ab.ShowDialog();
                     if (dr == DialogResult.Abort)
                         break;
                 }
+                if (updatedSongsCount == 0)
+                    MessageBox.Show("All selected songs seems to have lyrics");
+                else
+                    MessageBox.Show("Update completed");
             }
         }
 
